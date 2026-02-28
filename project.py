df1cf0ac7aa0c5efa31bf8426c16bca0b247924e
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-28 09:16:52 +0545

    price fetcher

--- project.py
+++ project.py
@@ -1,15 +1,15 @@
 import requests
 import sys
-
 def main():
     coin_input = input("Enter coin name or symbol (e.g., bitcoin, BTC): ").lower()
     if coin_input.isdigit():
          sys.exit("Invalid Input")
-    coin_price(coin_input)
+    print(coin_price(coin_input))
+    print(get_historical_prices(coin_input))
     
 
 def coin_price(input):
-    found = False
+    
     url = "https://rest.coincap.io/v3/assets?apiKey=353debcd0dbc55a8a2f151d3c6984de7d5b0a7cadb17d42a054ebf7ed3f2038b"
     response = requests.get(url)
     data = response.json()
@@ -18,10 +18,20 @@ def coin_price(input):
                 price = float(coin["priceUsd"])
                 found = True
                 return price
-         elif not found:
-              sys.exit("Unable to find the coin !")
-   
-if __name__ =="__main__":
-    main()
+    sys.exit("Unable to find the coin !")
 
+def get_historical_prices(asset_id, days=30):
+    url = f"https://rest.coincap.io/v3/assets/{asset_id}/history?interval=d1&limit={days}&apiKey=353debcd0dbc55a8a2f151d3c6984de7d5b0a7cadb17d42a054ebf7ed3f2038b"
 
+    response = requests.get(url)
+    data = response.json()
+
+    prices = []
+    dates = []
+
+    for item in data["data"]:
+         ...
+         
+
+if __name__ == "__main__":
+     main()
