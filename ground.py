6403f74148c8347d630dd976f8c08c464982df63
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-03-01 10:12:57 +0545

    fetching change within 24 hr

--- ground.py
+++ ground.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 def main():
-    coin_input = input("Enter coin name or symbol (e.g., bitcoin, BTC): ").lower()
+    coin_input = input("Enter coin name or symbol (e.g., bitcoin, BTC): ").lower().strip()
     if coin_input.isdigit():
          sys.exit("Invalid Input")
     url = "https://rest.coincap.io/v3/assets?apiKey=353debcd0dbc55a8a2f151d3c6984de7d5b0a7cadb17d42a054ebf7ed3f2038b"
@@ -12,20 +12,22 @@ def main():
 
 
 def coin_price(input,data):
-    
     found = False
+
     for item in data["data"]:
-        if input == item["id"].lower or input == item["symbol"].lower():
-            found = True
+        if input == item["id"].lower() or input == item["symbol"].lower():
             price = float(item["priceUsd"])
+            found = True
             return price
-            return found
-        if not found:
-            sys.exit("Coin not found !!")
+        
+    if not found:
+        sys.exit("Coudn't find the coin !")
+
 
 def change_within_24hr(input,data):
-    ...
-    
+    for item in data["data"]: 
+        change = item["changePercent24Hr"]  
+        return change 
         
 if __name__ == "__main__":
     main()
