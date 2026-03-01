875ae567b28e0e059167ebf774890b5e13d5b880
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-03-01 10:24:21 +0545

    ADDED RANK AND 24h change OF COIN

--- ground.py
+++ ground.py
@@ -7,26 +7,30 @@ def main():
     url = "https://rest.coincap.io/v3/assets?apiKey=353debcd0dbc55a8a2f151d3c6984de7d5b0a7cadb17d42a054ebf7ed3f2038b"
     response = requests.get(url)
     data = response.json()
-    print(f"The current price of {coin_input.upper()} is $",coin_price(coin_input,data))
-    print(f"24-Hour Change of {coin_input.upper()} :",change_within_24hr(coin_input,data))
+    print(f"{coin_input.upper()}: #{rank(coin_input,data)}")
+    print(f"Price: ${coin_price(coin_input,data)}")
+    print(f"24h change:",change_within_24hr(coin_input,data))
 
 
 def coin_price(input,data):
-    found = False
 
     for item in data["data"]:
         if input == item["id"].lower() or input == item["symbol"].lower():
             price = float(item["priceUsd"])
-            found = True
             return price
-        
-    if not found:
-        sys.exit("Coudn't find the coin !")
+    
+    sys.exit("Coudn't find the coin !")
+
+def rank(input,data):
+    for item in data["data"]:
+        rank = item["rank"]
+        return rank
 
 
 def change_within_24hr(input,data):
     for item in data["data"]: 
-        change = item["changePercent24Hr"]  
+        change = float(item["changePercent24Hr"])
+        change = round(change,3)
         return change 
         
 if __name__ == "__main__":
