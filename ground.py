b1bb46418494bf4e5eec1fac0463d3b5857cab86
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-03-11 23:06:54 +0545

    Coin Price and Coin Rank Feature

--- ground.py
+++ ground.py
@@ -1,1 +1,6 @@
 # https://rest.coincap.io/v3/assets?apiKey={api_key}"
+
+#     coin = input("Enter the coin: ")
+# url = "https://rest.coincap.io/v3/assets?apiKey=353debcd0dbc55a8a2f151d3c6984de7d5b0a7cadb17d42a054ebf7ed3f2038b"
+# data = requests.get(url)
+# formatted = data.json()
--- project.py
+++ project.py
@@ -1,15 +1,35 @@
 import requests 
 import os
+import sys
 from dotenv import find_dotenv,load_dotenv
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
+
 def main():
-     coin = input("Enter the coin: ")
-     api_key = os.getenv("API_KEY")
-     url =  "https://rest.coincap.io/v3/assets?apiKey={api_key}"
+     coin_input = input("Enter the coin: ").lower()
+     api_key = os.getenv("COINCAP_API_KEY")
+     url =  f"https://rest.coincap.io/v3/assets?apiKey={api_key}"
      data = requests.get(url)
-     print(data)
+     formatted = data.json()
+     dict_data = find_coin(coin_input,formatted)
+
+     print("<-------------------------->")
+     print(f"Rank: {rank_coin(dict_data)}")
+     print(f"Price: ${coin_price(dict_data)}")
+
+def find_coin(coin_input,formatted):
+     for item in formatted["data"]:
+          if coin_input== item["symbol"].lower() or coin_input == item["name"].lower():
+               return item
+     sys.exit("Invalid Coin! ")
+
+
+def rank_coin(dict_data):
+     return dict_data["rank"]
+
+def coin_price(dict_data):
+     return round(float(dict_data["priceUsd"]),2)
 
 
 if __name__ == "__main__":
