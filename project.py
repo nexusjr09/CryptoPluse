bc50b52695a97213ee9574f6cc645f2059418eed
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-03-12 10:18:10 +0545

    Added top 5 coins

--- project.py
+++ project.py
@@ -35,6 +35,7 @@ def main():
      type_text(f"Volume USD: {volume_usd(dict_data)}",0.03)
      type_text(f"24Hr Change percent: {change_percent(dict_data)}%",0.03)
      print("-------------------------------")
+     top5_coins(formatted)
 
 def find_coin(coin_input,formatted):
      for item in formatted["data"]:
@@ -67,6 +68,16 @@ def volume_usd(dict_data):
 
 def change_percent(dict_data):
      return round(float(dict_data["changePercent24Hr"]),3)
+
+def top5_coins(formatted):
+     data = input("Do you want to see the Top 5 Coins?(y/n): ").lower()
+     if data == "yes" or data == "y" or data == "yeh" or data == "sure":
+          for item in formatted["data"]:
+               if int(item["rank"])<=5:
+                    print(f"{item["rank"]}| {item["symbol"]} | ${round(float(item["priceUsd"]))} | {round(float(item["changePercent24Hr"]),3)}% ")
+          sys.exit()
+     sys.exit("Okay !!")
+
 
 
 
