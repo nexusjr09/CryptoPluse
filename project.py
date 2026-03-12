a17c7b56517f53ec68011c65ac1525a45269862e
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-03-12 09:11:28 +0545

    added multiple feature Related to coins

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import requests 
 import os
 import sys
+from numerize import numerize
 from dotenv import find_dotenv,load_dotenv
 
 dotenv_path = find_dotenv()
@@ -13,10 +14,16 @@ def main():
      data = requests.get(url)
      formatted = data.json()
      dict_data = find_coin(coin_input,formatted)
+     
 
      print("<-------------------------->")
      print(f"Rank: {rank_coin(dict_data)}")
      print(f"Price: ${coin_price(dict_data)}")
+     print(f"Circulating Supply: {supply(dict_data)}")
+     print(f"Max Supply: {maxsupply(dict_data)}")
+     print(f"Volume USD: {volume_usd(dict_data)}")
+     print(f"Change percent: {change_percent(dict_data)}%")
+
 
 def find_coin(coin_input,formatted):
      for item in formatted["data"]:
@@ -30,6 +37,19 @@ def rank_coin(dict_data):
 
 def coin_price(dict_data):
      return round(float(dict_data["priceUsd"]),2)
+
+def supply(dict_data):
+     return numerize.numerize(float(dict_data["supply"]))
+
+def maxsupply(dict_data):
+     return numerize.numerize(float(dict_data["maxSupply"]))
+
+def volume_usd(dict_data):
+     return numerize.numerize(float(dict_data["volumeUsd24Hr"]))
+
+def change_percent(dict_data):
+     return round(float(dict_data["changePercent24Hr"]),3)
+
 
 
 if __name__ == "__main__":
