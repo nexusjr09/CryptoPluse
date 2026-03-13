1df43f45f9606dd1da91de83ef2286496c5e4e2a
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-03-13 12:12:36 +0545

    All Bug fixes

--- project.py
+++ project.py
@@ -12,7 +12,7 @@ load_dotenv(dotenv_path)
 
 def main():
      
-     coin_input = input(Fore.RED+"Enter the coin: "+Style.RESET_ALL).lower().strip()
+     coin_input = input(Fore.GREEN+"Enter the coin: "+Style.RESET_ALL).lower().strip()
      api_key = os.getenv("COINCAP_API_KEY")
      url =  f"https://rest.coincap.io/v3/assets?apiKey={api_key}"
      try:
@@ -29,7 +29,7 @@ def main():
      
      dict_data = find_coin(coin_input,formatted)
 
-     print(Fore.GREEN+"\nFetching data....🔄"+Style.RESET_ALL)
+     print(Fore.RED+"\nFetching data....🔄"+Style.RESET_ALL)
      time.sleep(2)
      coin_stats = [
     ["Rank", rank_coin(dict_data)],
@@ -77,28 +77,30 @@ def change_percent(dict_data):
      return round(float(dict_data["changePercent24Hr"]),3)
 
 def top5_coins(formatted):
-     data = input(Fore.RED+"\nDo you want to see the Top 5 Coins currently in Market?(y/n): "+Style.RESET_ALL).lower()
+     data = input(Fore.GREEN+"\nDo you want to see the Top 5 Coins currently in Market?(y/n): "+Style.RESET_ALL).lower()
      if data == "yes" or data == "y" or data == "yeh" or data == "sure":
           my_list=[]
           for item in formatted["data"]:
                if int(item["rank"])<=5:
                     my_list.append([
     int(item["rank"]), #not a string so not using f" "
-    f"{item["symbol"]}",
-    f"${round(float(item["priceUsd"]),2)}",
-    f"{round(float(item["changePercent24Hr"]),2)}%"
+    item["symbol"],
+    f"${round(float(item['priceUsd']),2)}",
+    f"{round(float(item['changePercent24Hr']),2)}%"
 ])
           my_list.sort()
           headers = ["RANK","SYMBOL","PRICE IN USD","CHANGE IN %"]
           stats_table = tabulate(my_list,headers=headers,tablefmt="fancy_grid")
           print(stats_table)  
+     else:
+          print("Skipping Top 5 coins! ")
           
 
 def compare_coins(formatted):
-     data = input(Fore.RED+"\nDo you want to compare any two coins? (y/n)"+Style.RESET_ALL).strip().lower()
+     data = input(Fore.GREEN+"\nDo you want to compare any two coins? (y/n)"+Style.RESET_ALL).strip().lower()
      my_list = []
      if data == "y" or data == "yes":
-          coin1 = input(Fore.GREEN+"\nEnter coin1: "+Style.RESET_ALL).lower().strip()
+          coin1 = input(Fore.RED+"\nEnter coin1: "+Style.RESET_ALL).lower().strip()
           for item in formatted["data"]:
                if coin1 == item["name"].lower() or coin1 == item["symbol"].lower():
                     my_list.append([int(item["rank"]),item["name"],f"${round(float(item['priceUsd']),2)}",f"{round(float(item['changePercent24Hr']),2)}%"])
@@ -106,7 +108,7 @@ def compare_coins(formatted):
           else:
                sys.exit("Invalid coin 1🛑")
 
-          coin2 = input(Fore.GREEN+"Enter coin2: "+Style.RESET_ALL).lower().strip()
+          coin2 = input(Fore.RED+"Enter coin2: "+Style.RESET_ALL).lower().strip()
           for item in formatted["data"]:
                if coin2 == item["name"].lower() or coin2 == item["symbol"].lower():
                     my_list.append([int(item["rank"]),item["name"],f"${round(float(item['priceUsd']),2)}",f"{round(float(item['changePercent24Hr']),2)}%"])
