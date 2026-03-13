d6c2f51735a7e9adfe45bfbabf411fc54c7ffccb
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-03-13 10:38:52 +0545

    bug fixes

--- project.py
+++ project.py
@@ -98,15 +98,17 @@ def compare_coins(formatted):
      data = input(Fore.RED+"\nDo you want to compare any two coins? (y/n)"+Style.RESET_ALL).strip().lower()
      my_list = []
      if data == "y" or data == "yes":
-          coin1 = input(Fore.GREEN+"\nEnter coin1: "+Style.RESET_ALL)
+          coin1 = input(Fore.GREEN+"\nEnter coin1: "+Style.RESET_ALL).lower().strip()
           for item in formatted["data"]:
                if coin1 == item["name"].lower() or coin1 == item["symbol"].lower():
                     my_list.append([int(item["rank"]),item["name"],f"${round(float(item["priceUsd"]),2)}",f"{round(float(item["changePercent24Hr"]),2)}%"])
+               
                     coin2 = input(Fore.GREEN+"Enter coin2: "+Style.RESET_ALL).lower().strip()
                     for item in formatted["data"]:
                          if coin2 == item["name"].lower() or coin2 == item["symbol"].lower():
                               my_list.append([int(item["rank"]),item["name"],f"${round(float(item["priceUsd"]),2)}",f"{round(float(item["changePercent24Hr"]),2)}%"])
                               headers = ["RANK","NAME","PRICE IN USD","CHANGE PERCENT (24hr)"]
+                         sys.exit("Invalid Coin 2")
           stats_table = tabulate(my_list,headers=headers,tablefmt="fancy_grid")
           print(f"\n{stats_table}")
      sys.exit()
